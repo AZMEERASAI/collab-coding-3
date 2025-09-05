ba150e54820810edf0dd1c1bffb66ae31a32d4c6
bhanodhayrao <bhanodhayrao@gmail.com>
2025-09-05 13:46:12 +0530

    Contributor4: added gcd function (function4) and updated driver

--- main.py
+++ main.py
@@ -14,13 +14,17 @@ def function3():
     # Contributor 3 will implement
     pass
 
-def function4():
-    # Contributor 4 will implement
-    pass
+def function4(a: int, b: int) -> int:
+    """Returns GCD of two numbers."""
+    while b:
+        a, b = b, a % b
+    return a
 
 
 # Driver (Admin will finalize later)
 if __name__ == "__main__":
     print("Math Utilities Project Running...")
     print("Factorial of 5:", function1(5))
+    print("GCD of 48 and 18:", function4(48, 18))
     # Admin will call all contributed functions here
+
